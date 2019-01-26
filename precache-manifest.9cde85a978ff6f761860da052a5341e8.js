0e3ab9d6f2e34390b942a48dec3781049f6ca585
Herman Starikov <hermanstarikov@gmail.com>
2019-01-25 22:10:13 -0500

    Updates

---
+++ precache-manifest.9cde85a978ff6f761860da052a5341e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea12347b8d1e905fd4a2",
+    "url": "/use-typing-effect/static/js/runtime~main.ea12347b.js"
+  },
+  {
+    "revision": "340e69cb8598a7cef2ef",
+    "url": "/use-typing-effect/static/js/main.340e69cb.chunk.js"
+  },
+  {
+    "revision": "8c4a6f64e0f927034d8c",
+    "url": "/use-typing-effect/static/js/1.8c4a6f64.chunk.js"
+  },
+  {
+    "revision": "340e69cb8598a7cef2ef",
+    "url": "/use-typing-effect/static/css/main.d96547e4.chunk.css"
+  },
+  {
+    "revision": "25cd7c354d6e10c1832af6cfae4c6305",
+    "url": "/use-typing-effect/index.html"
+  }
+];
